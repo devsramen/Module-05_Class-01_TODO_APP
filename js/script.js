f4878491d47215ca458b12d07a8f7b5846a3027f
Ramen Biswas <printrb72@gmail.com>
2023-12-31 15:54:33 +0600

    update at 31-12-2023

--- js/script.js
+++ js/script.js
@@ -5,30 +5,55 @@ var newItemText = document.querySelector("#newItemText");
 var checkedBox = document.querySelector("#checkedBox");
 var deleteIcon = document.querySelector("#deleteIcon");
 
-// function te(){
-//     console.log(newItemText);
-// }
-// te();
+var statusDone = document.querySelector("#statusDone");
+var statusPending = document.querySelector("#statusPending");
+
 addNewItemBtn.addEventListener("click", function(){
-    var newPendingItem = document.createElement("li");
-    newPendingItem.classList.add("statusPending");
-    newPendingItem.innerHTML = `<div class="d-flex justify-content-between align-items-center">
-    <div>
-    <i id="statusDone" class="fa-solid fa-square-check"></i>
-        ${newItemText.value}
-    </div>
-    <i id="deleteIcon" class="fa-solid fa-trash-can"></i>
-</div>`
-    pendingListItemHolder.appendChild(newPendingItem);
+    if(newItemText.value === ''){
+        alert("You must write somethng!");
+    }else{
+        var newPendingItem = document.createElement("li");
+        newPendingItem.classList.add("statusPending");
+        newPendingItem.innerHTML = `<div class="d-flex justify-content-between align-items-center">
+        <div>
+        <i id="statusDone" class="fa-regular fa-circle"></i>
+            ${newItemText.value}
+        </div>
+        <i id="deleteIcon" class="fa-solid fa-trash-can"></i>
+    </div>`
+        pendingListItemHolder.appendChild(newPendingItem);        
+    }
 });
-addNewItemBtn.addEventListener("click", function(){
-    var newPendingItem = document.createElement("li");
-    newPendingItem.classList.add("statusWaitting");
-    newPendingItem.innerText= newItemText.value;
-    completedListItemHolder.appendChild(newPendingItem);
-    newItemText.value = "";
+// addNewItemBtn.addEventListener("click", function(){
+//     var newPendingItem = document.createElement("li");
+//     newPendingItem.classList.add("statusWaitting");
+//     newPendingItem.innerText= newItemText.value;
+//     completedListItemHolder.appendChild(newPendingItem);
+//     newItemText.value = "";
+// });
+
+
+// statusDone.addEventListener("click", function(){
+//     console.log("statusDone");
+// })
+
+statusDone.addEventListener("click", function(e){
+    let tag = e.target.tagName;
+    console.log(tag);
 });
 
 
-    
+
+
+// let ulMain = document.querySelector(".ulMain");
+// let liItem = document.querySelector(".liItem");
+
+
+
+// liItem.addEventListener("click", function(e){
+//     let tag = e.target.tagName;
+//     console.log(tag);
+// });
+
+
 
